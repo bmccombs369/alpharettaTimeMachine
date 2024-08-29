900de2c49e1368f466bd4d0e914c8df2c344308a
bmccombs369 <bmccombs369@gmail.com>
2024-08-29 12:39:30 -0400

    added search box to header for large screens, search button for smaller screen

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Navbar } from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link } from 'react-router-dom';
+import {AiOutlineSearch} from 'react-icons/ai';
 
 export default function Header() {
   return (
@@ -9,6 +10,17 @@ export default function Header() {
        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Alpharetta</span>
        Time Machine
       </Link>
+      <form>
+        <TextInput 
+          type='text'
+          placeholder='Search'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
     </Navbar>
   )
 }
